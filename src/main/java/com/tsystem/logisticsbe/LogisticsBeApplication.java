06fc2743ab279a82d03b314848a4c55688dff75f
Oleg Dubovitskiy <oleg.dubovitskiy@gmail.com>
2021-09-29 09:00:20 +0300

    Initial commit

---
+++ src/main/java/com/tsystem/logisticsbe/LogisticsBeApplication.java
@@ -0,0 +1,13 @@
+package com.tsystem.logisticsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsystem/logisticsbe/LogisticsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsystem.logisticsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
