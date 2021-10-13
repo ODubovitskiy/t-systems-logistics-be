390482315fae4f60c3eeb286f0f0a81c5558e7f8
Oleg Dubovitskiy <oleg.dubovitskiy@gmail.com>
2021-10-13 13:04:58 +0300

    Add opportunity to get cities from database
    Simple realization of getting cities list for different purposes. Exception handling is coming in the future commits.

---
+++ src/main/java/com/tsystem/logisticsbe/controller/CityController.java
@@ -0,0 +1,28 @@
+package com.tsystem.logisticsbe.controller;
+
+import com.tsystem.logisticsbe.dto.CityDTO;
+import com.tsystem.logisticsbe.factory.CityFactory;
+import com.tsystem.logisticsbe.service.ICityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CityController implements ICityController {
+
+    private final ICityService cityService;
+    private final CityFactory cityFactory;
+
+    @Autowired
+    public CityController(ICityService cityService, CityFactory cityFactory) {
+        this.cityService = cityService;
+        this.cityFactory = cityFactory;
+    }
+
+    @Override
+    public List<CityDTO> getAll() {
+        return cityFactory.createDefaultCityDTOList(cityService.getAll());
+    }
+}
---
+++ src/main/java/com/tsystem/logisticsbe/service/CityService.java
@@ -0,0 +1,24 @@
+package com.tsystem.logisticsbe.service;
+
+import com.tsystem.logisticsbe.entity.City;
+import com.tsystem.logisticsbe.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService implements ICityService {
+
+    private final CityRepository cityRepository;
+
+    @Autowired
+    public CityService(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
+    @Override
+    public List<City> getAll() {
+        return cityRepository.findAll();
+    }
+}
