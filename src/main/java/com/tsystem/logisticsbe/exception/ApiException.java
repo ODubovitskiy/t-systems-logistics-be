b632a4342ada4a76eb661efbd0d45acf91f32a95
Oleg Dubovitskiy <oleg.dubovitskiy@gmail.com>
2021-10-17 22:49:48 +0300

    Add general API Exception

--- src/main/java/com/tsystem/logisticsbe/exception/ApiException.java
+++ src/main/java/com/tsystem/logisticsbe/exception/ApiException.java
@@ -1,2 +1,28 @@
-package com.tsystem.logisticsbe.exception;public class ApiException {
+package com.tsystem.logisticsbe.exception;
+
+public class ApiException extends RuntimeException {
+
+    private static final String EXCEPTION_DESCRIPTION = "Unexpected error occurred in spring-boot-service";
+
+    private final Integer status;
+    private final String description;
+
+    public ApiException(Integer status, String description) {
+        super(description);
+        this.status = status;
+        this.description = description;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return getDescription();
+    }
+
+    public String getDescription() {
+        return description.isEmpty() ? EXCEPTION_DESCRIPTION : description;
+    }
 }
